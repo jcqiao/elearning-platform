84f086f63a0933a65914eb15d8b406713937c19e
cqiao <chunqiao21@gmail.com>
2024-08-15 19:28:00 -0700

    Layout showText

--- src/components/CourseLearnInteractive.js
+++ src/components/CourseLearnInteractive.js
@@ -1,41 +1,60 @@
 import React, { useState } from "react";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import img1 from "../video/1.png";
 import img2 from "../video/2.png";
 import { useNavigate } from "react-router-dom";
 
+const slides = [
+  {
+    content: "Slide 1",
+    imageUrl: img1,
+    buttons: [
+      {
+        id: 1,
+        label: "Button 1",
+        clicked: false,
+        text: "You click button 1 susccessfully"
+      },
+      {
+        id: 2,
+        label: "Button 2",
+        clicked: false,
+        text: "You click button 2 susccessfully"
+      },
+      {
+        id: 3,
+        label: "Button 3",
+        clicked: false,
+        text: "You click button 3 susccessfully"
+      }
+    ]
+  },
+  {
+    content: "Slide 2",
+    imageUrl: img2,
+    buttons: [
+      { id: 1, label: "Button 1", clicked: false },
+      { id: 2, label: "Button 2", clicked: false }
+    ]
+  }
+  // 其他幻灯片...
+];
 const CourseLearnInteractive = () => {
-  const slides = [
-    {
-      content: "Slide 1",
-      imageUrl: img1,
-      buttons: [
-        { id: 1, label: "Button 1", clicked: false },
-        { id: 2, label: "Button 2", clicked: false },
-        { id: 3, label: "Button 3", clicked: false }
-      ]
-    },
-    {
-      content: "Slide 2",
-      imageUrl: img2,
-      buttons: [
-        { id: 1, label: "Button 1", clicked: false },
-        { id: 2, label: "Button 2", clicked: false }
-      ]
-    }
-    // 其他幻灯片...
-  ];
-
   const [currentSlide, setCurrentSlide] = useState(0);
   const [buttonClicks, setButtonClicks] = useState(
     slides.map(slide => slide.buttons.map(button => false))
   );
+  const [showTexts, setShowTexts] = useState(slides.map(() => ""));
   const navigate = useNavigate();
 
   const handleButtonClick = (slideIndex, buttonIndex) => {
     const updatedClicks = [...buttonClicks];
     updatedClicks[slideIndex][buttonIndex] = true;
     setButtonClicks(updatedClicks);
+    // Update showText for the current slide
+    const updatedShowTexts = [...showTexts];
+    updatedShowTexts[slideIndex] = slides[slideIndex].buttons[buttonIndex].text;
+    setShowTexts(updatedShowTexts);
   };
 
   const handleNextSlide = () => {
@@ -65,7 +84,6 @@ const CourseLearnInteractive = () => {
         alignItems: "center",
         justifyContent: "center",
         height: "80vh",
-        color: "#fff",
         textAlign: "center"
       }}
     >
@@ -83,26 +101,44 @@ const CourseLearnInteractive = () => {
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
           borderRadius: 2,
-          marginBottom: 2
+          marginBottom: 2,
+          padding: 5
         }}
       >
-        {slides[currentSlide].buttons.map((button, index) =>
-          <Button
-            key={button.id}
-            variant="contained"
-            color={buttonClicks[currentSlide][index] ? "secondary" : "primary"}
-            onClick={() => handleButtonClick(currentSlide, index)}
-            sx={{
-              position: "absolute",
-              top: `${20 + index * 20}%`,
-              left: `${20 + index * 20}%`
-            }}
-          >
-            {button.label}
-          </Button>
-        )}
+        <Grid container spacing={2}>
+          <Grid item xs={6} md={6}>
+            <Grid
+              container
+              direction={"column"}
+              spacing={2}
+              alignItems={"start"}
+            >
+              {slides[currentSlide].buttons.map((button, index) =>
+                <Grid item xs={6}>
+                  <Button
+                    key={button.id}
+                    variant="contained"
+                    color={
+                      buttonClicks[currentSlide][index]
+                        ? "secondary"
+                        : "primary"
+                    }
+                    onClick={() => handleButtonClick(currentSlide, index)}
+                  >
+                    {button.label}
+                  </Button>
+                </Grid>
+              )}
+            </Grid>
+          </Grid>
+          <Grid item xs={6} md={6}>
+            {showTexts[currentSlide] &&
+              <Typography variant="body1" gutterBottom>
+                {showTexts[currentSlide]}
+              </Typography>}
+          </Grid>
+        </Grid>
       </Box>
-
       <Box
         sx={{ display: "flex", justifyContent: "space-between", width: "80%" }}
       >
